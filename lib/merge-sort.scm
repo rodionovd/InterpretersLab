17e6941c600e922fe9df8e8413afd05051b1c6ea
Dmitry Rodionov <i.am.rodionovd@gmail.com>
2015-12-18 01:59:13 +0600

    Include missing libraries
    Thanks .gitignore!

---
+++ lib/merge-sort.scm
@@ -0,0 +1,90 @@
+; =============
+; Merge sort
+; =============
+
+(begin
+
+	(import lib/stdlib)
+
+	; Merges two *sorted* lists in a single one (also sorted)
+	(define merge-lists (lambda (lhs rhs)
+		; if any of the list is empty we simply return the other one
+		(cond
+			((null? lhs) rhs)
+			((null? rhs) lhs)
+			(else
+				(if (>= (car lhs) (car rhs))
+					(cons (car rhs) (merge-lists lhs (cdr rhs)))
+					(cons (car lhs) (merge-lists (cdr lhs) rhs))
+			))
+		)
+		;
+		; An alternative implementation using `if` instead of `cond`
+		;
+		;(if (null? lhs)
+		;	rhs
+		;	(if (null? rhs)
+		;		lhs
+		;		(if (>= (car lhs) (car rhs))
+		;			(cons (car rhs) (merge-lists lhs (cdr rhs)))
+		;			(cons (car lhs) (merge-lists (cdr lhs) rhs))
+		;		)
+		;	)
+		;)
+	))
+
+	(assert (equal? (merge-lists '() '()) '()))
+	(assert (equal? (merge-lists '(1) '(2)) '(1 2)))
+	(assert (equal? (merge-lists '(2) '(1)) '(1 2)))
+	(assert (equal? (merge-lists '(2 5 9) '(1 2 10)) '(1 2 2 5 9 10)))
+	(assert (equal? (merge-lists '(2 7 100 101) '(0 5 9 12 105)) '(0 2 5 7 9 12 100 101 105)))
+
+	; Returns the right half of a list
+	(define right-half (lambda (list)
+		(last list (ceiling (/ (length list) 2)))
+	))
+
+	(assert (equal? (right-half '()) '()))
+	(assert (equal? (right-half '(1)) '(1)))
+	(assert (equal? (right-half '(1 3 4)) '(3 4)))
+	(assert (equal? (right-half '(6 5 4 3)) '(4 3)))
+	(assert (equal? (right-half '(6 5 4 3 0)) '(4 3 0)))
+
+	; Returns the left side of a list
+	(define left-half (lambda (list)
+		(ldiff list (right-half list))
+	))
+
+	(assert (equal? (left-half '()) '()))
+	(assert (equal? (left-half '(1)) '()))
+	(assert (equal? (left-half '(1 3 4)) '(1)))
+	(assert (equal? (left-half '(6 5 4 3)) '(6 5)))
+	(assert (equal? (left-half '(6 5 4 3 0)) '(6 5)))
+
+	; Returns T if the given list is empty or contains just one element
+	(define small (lambda (list)
+		(or
+			(= (length list) 0)
+			(= (length list) 1)
+		)
+	))
+
+	(assert (equal? (small '()) 1))
+	(assert (equal? (small '(foo)) 1))
+	(assert (equal? (small '(foo bar)) 0))
+
+	; Sorts the given list using merge sort algorithm
+	(define merge-sort (lambda (list)
+		(if (small list)
+			list ; return the list itself if it contains one or zero items
+			(merge-lists ; otherwise merge the left and the right halfs (already sorted) of the list recursively
+				(merge-sort (left-half list))
+				(merge-sort (right-half list))
+			)
+		)
+	))
+
+	(assert (equal? (merge-sort '()) '()))
+	(assert (equal? (merge-sort '(1)) '(1)))
+	(assert (equal? (merge-sort '(1 -8 5 19 7 3 4 9)) '(-8 1 3 4 5 7 9 19)))
+)
---
+++ lib/stdlib.scm
@@ -0,0 +1,339 @@
+; =============
+; stdlib
+; =============
+
+(begin
+
+	; ========================================================
+	; COMPARATORS
+	; ========================================================
+
+	; Greater than or equal
+	(define >= (lambda (lhs rhs)
+		(or (> lhs rhs) (= lhs rhs))
+	))
+
+	; Less than or equal
+	(define <= (lambda (lhs rhs)
+		(or (< lhs rhs) (= lhs rhs))
+	))
+
+	; Returns T if the given list is empty
+	(define null? (lambda (list)
+		(= list '())
+	))
+
+	(define equal? (lambda (lhs rhs)
+		(if (= lhs rhs) #t #f)
+	))
+
+	(define not (lambda (x)
+		(if (equal? #f x) #t #f)
+	))
+
+	(assert (equal? (not #f) #t))
+	(assert (equal? (not #t) #f))
+	(assert (equal? (not 3) #f))
+	(assert (equal? (not '(3)) #f))
+	(assert (equal? (not '()) #f))
+
+	; ========================================================
+	; TYPES
+	; ========================================================
+
+	(assert (equal? (boolean? #t) #t))
+	(assert (equal? (boolean? #f) #t))
+	(assert (equal? (boolean? '()) #f))
+	(assert (equal? (boolean? 0) #f))
+	(assert (equal? (boolean? <=) #f))
+
+	; ========================================================
+	; LISTS
+	; ========================================================
+
+	; ------------------
+	; <reverse>
+	; ------------------
+	; Returns a reversed list.
+	; If x isn't a list, this function is no-op
+	(define reverse (lambda (x)
+		(if (or (null? x) (not (list? x)))
+			x
+			(cons (reverse (cdr x)) (car x))
+		)
+	))
+
+	(assert (equal? (reverse '()) '()))
+	(assert (equal? (reverse '(42)) '(42)))
+	(assert (equal? (reverse '(1 2 3)) '(3 2 1)))
+	(assert (equal? (reverse '(2 3 3)) '(3 3 2)))
+
+	; ------------------
+	; <foldr>
+	; ------------------
+	; Fold to the right.
+	; What kind of value it will return depend entirely on
+	; the accumulator and the function
+	(define foldr (lambda (func acc list)
+		(if (null? list)
+			acc
+			(foldr
+				func
+				(func acc (car list)) ; acc += item
+				(cdr list)
+			)
+		)
+	))
+
+	(assert (equal? (foldr + 0 '(1 2 3)) 6))
+	(assert (equal? (foldr cons '() '(1 2 3)) '(1 2 3)))
+
+	; ------------------
+	; <foldl>
+	; ------------------
+	; Fold to the left. See also: foldr
+	(define foldl (lambda (func acc list)
+		(foldr func acc (reverse list))
+	))
+
+	(assert (equal? (foldl + 0 '(1 2 3)) 6))
+	(assert (equal? (foldl cons '() '(1 2 3)) '(3 2 1)))
+	(assert (equal? (foldl (lambda (acc item) (cons item acc)) '() '(1 2 3)) '(1 2 3)))
+
+	; ------------------
+	; <length>
+	; ------------------
+	; Returns the length of an object.
+	(define length (lambda (x)
+		(foldr (lambda (acc _) (+ acc 1)) 0 x)
+	))
+
+	; ------------------
+	; <Map>
+	; Given a function `f` transforms (a b c) into ((f a) (f b) (f c)).
+	; ------------------
+
+	(define map (lambda (f list)
+		(foldr
+			(lambda (acc item)
+				(cons acc (f item)))
+			'()
+			list)
+	))
+
+	(assert (equal? (map length '((1 2 3) (0 -1 12 3) (3))) '(3 4 1)))
+	(assert (equal? (map null? '(() (1) () (2))) '(1 0 1 0)))
+
+	; ------------------
+	; <Count>
+	; Returns a number of times `item` appears in `list`
+	; ------------------
+
+	; foldr-based implementation for lulz
+	;(define count (lambda (subject list)
+	;	(foldr
+	;		(lambda (acc item)
+	;			(+ acc (if (equal? subject item) 1 0))
+	;		)
+	;		0 list)
+	;))
+
+	(define count (lambda (item list)
+		(if (null? list)
+			0
+			(+ (equal? item (car list)) (count item (cdr list)))
+		)
+	))
+
+	(assert (equal? 2 (count 1 '(1 8 7 6 1 9 0))))
+	(assert (equal? 3 (count 'foo '(afoo foo bar io foo fooo foo))))
+
+	; ------------------
+	; <last>
+	; ------------------
+	; Returns a list containing last `y` items of `list`.
+	; If y >= (length list) then list is original list is returned instead
+	(define last (lambda (list y)
+		(if (<= (length list) y)
+			list
+			(last (cdr list) y)
+		)
+	))
+
+	(assert (equal? (last '(8 7) 5) '(8 7)))
+	(assert (equal? (last '(8 7) 2) '(8 7)))
+	(assert (equal? (last '(8 7 6 510 31) 3) '(6 510 31)))
+
+	; ------------------
+	; <ldiff>
+	; ------------------
+	(define ldiff (lambda (lhs rhs)
+		(if (or (null? lhs) (equal? lhs rhs))
+			'()
+			(cons (car lhs) (ldiff (cdr lhs) rhs))
+		)
+	))
+
+	(assert (equal? (ldiff '(3 8 9 1) '()) '(3 8 9 1)))
+	(assert (equal? (ldiff '(3 8 9 1) '(1)) '(3 8 9)))
+	(assert (equal? (ldiff '(3 8 9 1) '(9 1)) '(3 8)))
+	(assert (equal? (ldiff '(3 8 9 1) '(8 9 1)) '(3)))
+	(assert (equal? (ldiff '(3 8 9 1) '(9 2)) '(3 8 9 1)))
+	(assert (equal? (ldiff '(3 8 9 1) '(0 0)) '(3 8 9 1)))
+
+	; ------------------
+	; <apply>
+	; ------------------
+	(define apply (lambda (func args)
+		(foldr func (car args) (cdr args))
+	))
+
+	(assert (equal? (apply + '(2 3 4)) 9))
+	(assert (equal? (apply cons '((1 2 3) (3) (4))) '(1 2 3 3 4)))
+
+	; ------------------
+	; <curry> (two args)
+	; ------------------
+
+	; TODO: implement many args curring (recursively?)
+	(define curry (lambda (func arg1)
+		(lambda (arg2)
+			(apply func (cons arg1 arg2))
+		)
+	))
+
+	(define zero? (lambda (x) ((curry = 0) x)))
+	(define positive? (lambda (x) ((curry < 0) x)))
+	(define negative? (lambda (x) ((curry > 0) x)))
+
+	(assert (equal? (zero? -1) #f))
+	(assert (equal? (zero? 0) #t))
+	(assert (equal? (zero? 1) #f))
+
+	(assert (equal? (positive? -1) #f))
+	(assert (equal? (positive? 0) #f))
+	(assert (equal? (positive? 1) #t))
+
+	(assert (equal? (negative? -1) #t))
+	(assert (equal? (negative? 0) #f))
+	(assert (equal? (negative? 1) #f))
+
+
+	; ========================================================
+	; MATH
+	; ========================================================
+
+	; Some useful constants
+	(define EPS 0.000001)
+	(define PI 3.141592653589793)
+
+	; Calculates a factorial of `n`
+	; n >= 0
+	(define fact (lambda (n)
+		(begin
+			;; define a helper function for accumulating results
+			(define factorial (lambda (n acc)
+				(if (= n 0)
+					acc
+					(factorial (- n 1) (* n acc))
+				)
+			))
+			; call the helper
+			(factorial n 1)
+		)
+	))
+
+	(assert (equal? (fact 0) 1))
+	(assert (equal? (fact 1) 1))
+	(assert (equal? (fact 6) 720))
+	(assert (equal? (fact 50) 30414093201713378043612608166064768844377641568960512000000000000))
+
+	; Returns x raised to the power of y.
+	; y must be >= 0
+	(define ^ (lambda (x y)
+		(if (equal? y 0)
+			1
+			; TODO: optimize for x=0
+			(if (equal? y 1)
+				x
+				(* x (^ x (- y 1)))
+			)
+		)
+	))
+
+	(assert (equal? (^ 0 0) 1))
+	(assert (equal? (^ 0 9) 0))
+	(assert (equal? (^ 2 0) 1))
+	(assert (equal? (^ 2 1) 2))
+	(assert (equal? (^ 2 10) 1024))
+	(assert (equal? (^ -2 2) 4))
+	(assert (equal? (^ -2 3) -8))
+
+	; x*x, x^2
+	(define square (lambda (x)
+		(^ x 2)
+	))
+
+	(assert (equal? (square 2) 4))
+	(assert (equal? (square -2) 4))
+	(assert (equal? (square 0) 0))
+
+	(define abs (lambda (x)
+		(if (< x 0) (- 0 x) x)
+	))
+
+	(assert (equal? (abs 3) (abs -3)))
+	(assert (equal? (abs 0) 0))
+
+	; Each iteration of the algorithm improves the guess for a litle
+	(define sqrt-iter (lambda (guess x)
+		(if (newton-sqrt-good-enough? guess x)
+			guess
+			(sqrt-iter (improve-newton-sqrt-guess guess x) x)
+		)
+	))
+
+	; new_guess = (old_guess + (x/old_guess)) / 2
+	(define improve-newton-sqrt-guess (lambda (guess x)
+		(average guess (/ x guess))
+	))
+
+	; Returns T if the difference between guess^2 and x is less than EPS
+	(define newton-sqrt-good-enough? (lambda (guess x)
+		(< (abs (- (square guess) x)) EPS)
+	))
+
+	; Return the average of two numbers
+	(define average (lambda (x y)
+		(/ (+ x y) 2)
+	))
+
+	; Returns a square root of x. Based on Newton's method.
+	; x >= 0 since we don't support complex numbers (yet?)
+	(define sqrt (lambda (x)
+		(if (equal? x 0)
+			0
+			(sqrt-iter 1.0 x)
+		)
+	))
+
+	(assert (and (> (sqrt 9) 3) (< (sqrt 9) (+ 3 EPS))))
+	(assert (equal? (sqrt 0) 0))
+
+	; Modulo
+	(define modulo (lambda (x y)
+		(- x (* y (flooring (/ x y))))
+	))
+
+	(define remainder modulo)
+
+	(assert (equal? (modulo 0 1) 0))
+	(assert (equal? (modulo 1 1) 0))
+	(assert (equal? (modulo 3 2) 1))
+	(assert (equal? (modulo 12 5) 2))
+	(assert (equal? (modulo -12 5) 3))
+	(assert (equal? (modulo 12 5) 2))
+	(assert (equal? (modulo -78 33) 21))
+	(assert (equal? (modulo 78 33) 12))
+	(assert (equal? (modulo 5 89) 5))
+)
