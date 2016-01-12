cd663ba70276265837d7de202d2d3c854d5e3986
Dmitry Rodionov <i.am.rodionovd@gmail.com>
2016-01-13 01:59:52 +0600

    Reformat inline assertions for `odd?` and `even?`

--- lib/stdlib.scm
+++ lib/stdlib.scm
@@ -178,15 +178,25 @@
 		(equal? (abs (remainder x 2)) 1)
 	))
 
-	(assert (odd? 1)) (assert (odd? 3)) (assert (odd? 1813))
-	(assert (not (odd? 2))) (assert (not (odd? 8724))) (assert (not (odd? 0)))
+	(assert (odd? 1))
+	(assert (odd? 3))
+	(assert (odd? 1813))
+
+	(assert (not (odd? 2)))
+	(assert (not (odd? 8724)))
+	(assert (not (odd? 0)))
 
 	(define even? (lambda (x)
 		(equal? (abs (remainder x 2)) 0)
 	))
 
-	(assert (even? 2)) (assert (even? 8724)) (assert (even? 0))
-	(assert (not (even? 3))) (assert (not (even? 8971))) (assert (not (even? 1)))
+	(assert (even? 2))
+	(assert (even? 8724))
+	(assert (even? 0))
+
+	(assert (not (even? 3)))
+	(assert (not (even? 8971)))
+	(assert (not (even? 1)))
 
 	; ========================================================
 	; LISTS
